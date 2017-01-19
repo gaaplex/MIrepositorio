15e7bb974e2a3a18e2bcd51eeeb802b4df393462
gaapl <gaapl@DESKTOP-FQJ8MBV>
2017-01-18 19:34:48 -0500

    Otra linea

--- src/mirepo/MIREPO.java
+++ src/mirepo/MIREPO.java
@@ -16,6 +16,10 @@ public class MIREPO {
      */
     public static void main(String[] args) {
         System.out.println("Ya se crear un repositorio");
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            
+        }
         }
     
 }
